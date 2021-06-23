e7c8e68340042636cfbbbf6f4cf94ad67c9d0123
Lightning Auriga <lightning.auriga@gmail.com>
2021-06-23 10:11:37 -0400

    docs: split documentation for text editing functions

--- R/string_vector_cleanup.R
+++ R/string_vector_cleanup.R
@@ -5,10 +5,79 @@
 #'
 #' - convert all characters to lower case
 #'
+#' @description
+#'
+#' @param df data frame, input phenotype content
+#' @return modified version of input with values cleaned as described
+#' above
+#' @export make.lowercase
+#' @examples
+#' phenotype.data <- data.frame(
+#'   c("WeiRd CaPs", "Trailing ", "NA"),
+#'   c(";something", "Not Applicable", "Too   muchspace")
+#' )
+#' colnames(phenotype.data) <- c("col1", "col2")
+#' phenotype.data <- make.lowercase(phenotype.data)
+make.lowercase <- function(df) {
+  df <- data.frame(lapply(df, function(val) {
+    tolower(val)
+  }))
+}
+#' Basic global cleanup of entries in a phenotype data frame
+#'
+#' @details
+#' Performs the following data modifications to non-numeric values:
+#'
 #' - remove all trailing and preceding whitespaces and collapse
 #'   consecutive whitespaces
 #'
+#' @description
+#'
+#' @param df data frame, input phenotype content
+#' @return modified version of input with values cleaned as described
+#' above
+#' @export remove.whitespace
+#' @examples
+#' phenotype.data <- data.frame(
+#'   c("WeiRd CaPs", "Trailing ", "NA"),
+#'   c(";something", "Not Applicable", "Too   muchspace")
+#' )
+#' colnames(phenotype.data) <- c("col1", "col2")
+#' phenotype.data <- remove.whitespace(phenotype.data)
+remove.whitespace <- function(df) {
+  df <- data.frame(lapply(df, function(val) {
+    stringr::str_squish(val)
+  }))
+}
+#' Basic global cleanup of entries in a phenotype data frame
+#'
+#' @details
+#' Performs the following data modifications to non-numeric values:
+#'
 #' - remove trailing and preceding non-alphanumerics
+#'
+#' @description
+#'
+#' @param df data frame, input phenotype content
+#' @return modified version of input with values cleaned as described
+#' above
+#' @export remove.nonword.chars
+#' @examples
+#' phenotype.data <- data.frame(
+#'   c("WeiRd CaPs", "Trailing ", "NA"),
+#'   c(";something", "Not Applicable", "Too   muchspace")
+#' )
+#' colnames(phenotype.data) <- c("col1", "col2")
+#' phenotype.data <- remove.nonword.chars(phenotype.data)
+remove.nonword.chars <- function(df) {
+  df <- data.frame(lapply(df, function(val) {
+    stringr::str_replace_all(val, "^\\W+|\\W+$", "")
+  }))
+}
+#' Basic global cleanup of entries in a phenotype data frame
+#'
+#' @details
+#' Performs the following data modifications to non-numeric values:
 #'
 #' - harmonize na, nan, not applicable values (unknowns?  blanks?)
 #'
@@ -17,9 +86,6 @@
 #' @param df data frame, input phenotype content
 #' @return modified version of input with values cleaned as described
 #' above
-#' @export make.lowercase
-#' @export remove.whitespace
-#' @export remove.nonword.chars
 #' @export normalize.missing.values
 #' @examples
 #' phenotype.data <- data.frame(
@@ -27,25 +93,7 @@
 #'   c(";something", "Not Applicable", "Too   muchspace")
 #' )
 #' colnames(phenotype.data) <- c("col1", "col2")
-#' phenotype.data <- make.lowercase(phenotype.data)
-#' phenotype.data <- remove.whitespace(phenotype.data)
-#' phenotype.data <- remove.nonword.chars(phenotype.data)
 #' phenotype.data <- normalize.missing.values(phenotype.data)
-make.lowercase <- function(df) {
-  df <- data.frame(lapply(df, function(val) {
-    tolower(val)
-  }))
-}
-remove.whitespace <- function(df) {
-  df <- data.frame(lapply(df, function(val) {
-    stringr::str_squish(val)
-  }))
-}
-remove.nonword.chars <- function(df) {
-  df <- data.frame(lapply(df, function(val) {
-    stringr::str_replace_all(val, "^\\W+|\\W+$", "")
-  }))
-}
 normalize.missing.values <- function(df) {
   df <- data.frame(lapply(df, function(val) {
     stringr::str_replace_all(val,
