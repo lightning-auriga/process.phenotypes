1d8d5ddc77934ecd10da287d9c39a66774d2f54a
Lightning Auriga <lightning.auriga@gmail.com>
2021-06-29 11:39:31 -0400

    feat: draft yaml loader function and test harmonizing shared models
    moving in the direction of yaml configuration of the entire process, this commit adds a function to
    load dataset-specific and shared model information from yaml configuration files. the function is
    not yet called in main execution. the basic method functionality is tested but more tests should be
    added for resolving various kinds of corner cases.

---
+++ R/yaml_configuration.R
@@ -0,0 +1,54 @@
+#' Load yaml configuration from file, integrating global models
+#'
+#' @description
+#'
+#' @details
+#'
+#' @param dataset.parameter.filename character vector, filename
+#' of dataset-specific variable parameters
+#' @param global.parameter.filename character vector, filename
+#' of global shared model specifications
+#' @return list of lists, containing parsed yaml configuration
+#' information
+#' @keywords yaml
+#' @export load.configuration
+load.configuration <- function(dataset.parameter.filename,
+                               global.parameter.filename) {
+  ## input parameter testing
+  stopifnot(
+    is.vector(dataset.parameter.filename, mode = "character"),
+    length(dataset.parameter.filename) == 1
+  )
+  stopifnot(
+    is.vector(global.parameter.filename, mode = "character"),
+    length(global.parameter.filename) == 1
+  )
+  ## read global shared model information
+  global.parameters <- yaml::read_yaml(global.parameter.filename)
+  ## input format testing for global model parameters
+  stopifnot(!is.null(global.parameters$models))
+  ## read project-specific variable parameter information
+  dataset.parameters <- yaml::read_yaml(dataset.parameter.filename)
+  ## input format testing for project-specific variable parameters
+  stopifnot(
+    !is.null(dataset.parameters$tag),
+    !is.null(dataset.parameters$variables)
+  )
+  ## apply shared model data when requested
+  for (var.name in names(dataset.parameters$variables)) {
+    print(dataset.parameters$variables[[var.name]])
+    requested.model <- dataset.parameters$variables[[var.name]][["shared_model"]]
+    if (!is.null(requested.model)) {
+      out.model <- global.parameters$models[[requested.model]]
+      stopifnot(!is.null(out.model))
+      ## apply dataset-specific values on top of any colliding variables in the
+      ## global specification
+      for (parameter.name in names(dataset.parameters$variables[[var.name]])) {
+        out.model[[parameter.name]] <- dataset.parameters$variables[[var.name]][[parameter.name]]
+      }
+      dataset.parameters$variables[[var.name]] <- out.model
+    }
+  }
+  ## return synthesized variable configuration data
+  dataset.parameters
+}
---
+++ tests/testthat/test_yaml_configuration.R
@@ -0,0 +1,29 @@
+test_that("dataset parameters loaded from yaml and updated with shared models", {
+  in.dataset.filename <- "files/yaml_configuration/dataset-parameters.yaml"
+  in.shared.models <- "files/yaml_configuration/shared-models.yaml"
+
+  out.list <- list(
+    tag = "testset",
+    variables = list(
+      var001 = list(
+        name = "PID",
+        type = "string",
+        subject_id = TRUE
+      ),
+      var013 = list(
+        type = "binary",
+        reference = "no",
+        comparison = "yes",
+        name = "Any associated goitre (neck swelling):",
+        shared_model = "yes_no"
+      )
+    )
+  )
+  expect_identical(
+    load.configuration(
+      in.dataset.filename,
+      in.shared.models
+    ),
+    out.list
+  )
+})
