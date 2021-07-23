90541eb289be4de9901f91f5218b37294d93f4fb
Lightning Auriga <lightning.auriga@gmail.com>
2021-07-23 11:47:58 -0400

    test: implement a half broken initial test for derived variables

--- tests/testthat/test_derived_variables.R
+++ tests/testthat/test_derived_variables.R
@@ -1,5 +1,27 @@
 test_that("simple derived variable calculation works", {
-
+  in.phenotype.data <- data.frame("PPB001" = 1:5)
+  in.variable.summary <- list(
+    variables = list("PPB001" = list(
+      name = "example variable 1",
+      type = "numeric"
+    )),
+    derived = list("PPB001_derived" = list(
+      name = "derived variable 1",
+      type = "numeric",
+      code = "PPB001 * 2"
+    ))
+  )
+  out.phenotype.data <- in.phenotype.data
+  out.phenotype.data[["PPB001_derived"]] <- out.phenotype.data$PPB001 * 2
+  out.variable.summary <- in.variable.summary
+  out.variable.summary$variables[["PPB001_derived"]] <- out.variable.summary$derived[["PPB001_derived"]]
+  expect_identical(
+    create.derived.variables(in.phenotype.data, in.variable.summary),
+    list(
+      phenotype.data = out.phenotype.data,
+      variable.summary = out.variable.summary
+    )
+  )
 })
 
 test_that("derived variable code cannot overwrite phenotype data", {
