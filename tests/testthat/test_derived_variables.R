e100ea271fb2743d0329e2d52b8594b95c9d5f2f
Lightning Auriga <lightning.auriga@gmail.com>
2021-07-23 11:04:02 -0400

    test: add placeholders for derived variable tests

---
+++ tests/testthat/test_derived_variables.R
@@ -0,0 +1,15 @@
+test_that("simple derived variable calculation works", {
+
+})
+
+test_that("derived variable code cannot overwrite phenotype data", {
+
+})
+
+test_that("derived variables may depend on other derived variables", {
+
+})
+
+test_that("derived variable blocks with bugs crash gracefully", {
+
+})
