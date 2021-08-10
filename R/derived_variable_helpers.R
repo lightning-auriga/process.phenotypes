171bd3cc66a8dccfdca43501d009c9aa96f69f9e
Lightning Auriga <lightning.auriga@gmail.com>
2021-08-10 14:29:05 -0400

    feat: support rank normal transform, and test its functionality

--- R/derived_variable_helpers.R
+++ R/derived_variable_helpers.R
@@ -38,3 +38,98 @@ derive.first.degree <- function(variable.name, derived.name) {
   derived.name[!is.na(variable.name) & !pat] <- "no"
   derived.name
 }
+
+#' Apply inverse/rank normal transformation to numeric variable
+#'
+#' @details
+#' Transforms a numeric vector to a normally distributed variable
+#' with certain configurable settings.
+#'
+#' @description
+#' The outcome of this transformation is guaranteed to be normal,
+#' but may not behave as you want it to. Parametric information
+#' about the variable is lost. Ties are resolved by random resolution,
+#' which has desirable statistical properties but which breaks down
+#' (as do all methods) as the number of ties increases relative to
+#' the sample size. Multiple transformations result in different
+#' variables, with the extent of the differences again dependent
+#' on the number of ties.
+#'
+#' @param variable numeric vector, input variable
+#' for transformation; using tidyverse masking, this can be
+#' referred to by the name of a variable in the input config
+#' @param offset numeric, small decimal offset for transform
+#' adjustment
+#' @param stratification.vars list, input variables to be used
+#' as factors for stratifying data before applying inverse
+#' normal transform; using tidyverse masking, these can be
+#' referred to by the names of variables in the input config
+#' @param include.subjects logical vector or NA, indicator vector for
+#' which subjects should be considered for current transformation
+#' @param primary.call logical, whether this is the top-level
+#' call of this function. do not set or change this, unless you want really
+#' awful behavior
+#' @export derive.rank.normal.transform
+derive.rank.normal.transform <- function(variable,
+                                         offset = 0.5,
+                                         stratification.vars = list(),
+                                         include.subjects = NA,
+                                         primary.call = TRUE) {
+  stopifnot(is.vector(variable, mode = "numeric"))
+  stopifnot(
+    is.numeric(offset),
+    length(offset) == 1
+  )
+  stopifnot(is.list(stratification.vars) | length(stratification.vars) == 0)
+  for (var in stratification.vars) {
+    stopifnot(is.factor(var))
+  }
+  stopifnot((is.vector(include.subjects, mode = "logical") & length(include.subjects) == length(variable)) |
+    isTRUE(all.equal(is.na(include.subjects), rep(TRUE, length(include.subjects)))))
+  stopifnot(
+    is.logical(primary.call),
+    length(primary.call) == 1
+  )
+  ## if the user hasn't specified an inclusion set, specify all subjects
+  if (isTRUE(all.equal(is.na(include.subjects), rep(TRUE, length(include.subjects))))) {
+    include.subjects <- rep(TRUE, length(variable))
+  }
+  ## if there are stratification variables left, recurse
+  res <- rep(NA, length(variable))
+  if (length(stratification.vars) > 0) {
+    ## just stratify by the first one; leave the rest to recursion
+    for (lvl in levels(stratification.vars[[1]])) {
+      subset.inclusion <- include.subjects & stratification.vars[[1]] == lvl
+      recurse.result <- derive.rank.normal.transform(
+        variable,
+        offset,
+        stratification.vars[-1],
+        subset.inclusion,
+        FALSE
+      )
+      res[subset.inclusion & is.na(res)] <- recurse.result[subset.inclusion & is.na(res)]
+    }
+  } else {
+    if (length(which(!is.na(variable[include.subjects]))) == 0) {
+      ## all included subjects are NA, set them to NA
+      res[include.subjects] <- rep(NA, length(which(include.subjects)))
+    } else {
+      ## some included subjects are not NA, transform them
+      res[include.subjects] <- qnorm((rank(variable[include.subjects],
+        na.last = "keep",
+        ties.method = "random"
+      ) - offset) /
+        sum(!is.na(variable[include.subjects])))
+    }
+  }
+  ## if stratification was requested, run a final pass to ensure
+  ## the overlaid distributions behave correctly
+  if (primary.call & length(stratification.vars) > 0) {
+    res <- derive.rank.normal.transform(
+      res,
+      offset,
+      list()
+    )
+  }
+  res
+}
--- tests/testthat/test_derived_variable_helpers.R
+++ tests/testthat/test_derived_variable_helpers.R
@@ -29,3 +29,172 @@ test_that("conversion from family members to yes/no first degree relative works"
     )
   )
 })
+
+
+test_that("rank normal transform works at all", {
+  in.phenotype.data <- data.frame(TV0001 = c(1, 2, 3, 4, 5))
+  in.variable.summary <- list(
+    variables = list(TV0001 = list(params = list(
+      name = "test variable",
+      type = "numeric"
+    ))),
+    derived = list(TV0001_derived = list(
+      name = "normal transform",
+      code = "derive.rank.normal.transform(TV0001, offset = 0.5)"
+    ))
+  )
+  out.phenotype.data <- in.phenotype.data
+  out.phenotype.data[, "TV0001_derived"] <- c(-1.2815516, -0.5244005, 0, 0.5244005, 1.2815516)
+  out.variable.summary <- in.variable.summary
+  out.variable.summary$variables[["TV0001_derived"]] <- list(
+    original.name = in.variable.summary$derived[["TV0001_derived"]]$name,
+    params = in.variable.summary$derived[["TV0001_derived"]]
+  )
+  expect_equal(create.derived.variables(
+    in.phenotype.data,
+    in.variable.summary
+  ),
+  list(
+    phenotype.data = out.phenotype.data,
+    variable.summary = out.variable.summary
+  ),
+  tolerance = 1e-5
+  )
+})
+
+test_that("rank normal transform returns NA vector with NA input", {
+  expect_identical(
+    derive.rank.normal.transform(as.numeric(rep(NA, 100))),
+    rep(NA, 100)
+  )
+})
+
+test_that("rank normal transform resolves ties randomly", {
+  x <- c(1, 2, 2, 3, 4, 5)
+  y.expected <- c(
+    -1.3829941, -0.2104284, -0.6744898,
+    0.2104284, 0.6744898, 1.3829941
+  )
+  n.random.mismatch <- 0
+  n.consistent.hits <- 0
+  n.total <- 100
+  for (i in seq_len(n.total)) {
+    y <- derive.rank.normal.transform(x)
+    if (isTRUE(all.equal(abs(y[c(-2, -3)] - y.expected[c(-2, -3)]) < 1e-5, rep(TRUE, 4)))) {
+      n.consistent.hits <- n.consistent.hits + 1
+    }
+    if (abs(y[2] - y.expected[2]) > 1e-5 |
+      abs(y[3] - y.expected[3]) > 1e-5) {
+      n.random.mismatch <- n.random.mismatch + 1
+    }
+  }
+  expect_equal(n.consistent.hits, n.total)
+  expect_true(binom.test(n.random.mismatch, n.total, 0.5, alt = "two")$p.value >= 0.01)
+})
+
+test_that("rank normal transform stratifies by one variable", {
+  in.phenotype.data <- data.frame(
+    TV0001 = c(1, 2, 3, 4, 5),
+    TVSTRAT = factor(c(1, 1, 1, 2, 2))
+  )
+  in.variable.summary <- list(
+    variables = list(
+      TV0001 = list(params = list(
+        name = "test variable",
+        type = "numeric"
+      )),
+      TVSTRAT = list(params = list(
+        name = "stratification variable",
+        type = "ordinal",
+        levels = list(
+          "1" = list(name = "1"),
+          "2" = list(name = "2")
+        )
+      ))
+    ),
+    derived = list(TV0001_derived = list(
+      name = "normal transform",
+      code = "derive.rank.normal.transform(TV0001, strat = list(TVSTRAT), offset = 0.5)"
+    ))
+  )
+  out.phenotype.data <- in.phenotype.data
+  out.phenotype.data[, "TV0001_derived"] <- c(-1.2815516, 0.0000000, 1.2815516, -0.5244005, 0.5244005)
+  out.variable.summary <- in.variable.summary
+  out.variable.summary$variables[["TV0001_derived"]] <- list(
+    original.name = in.variable.summary$derived[["TV0001_derived"]]$name,
+    params = in.variable.summary$derived[["TV0001_derived"]]
+  )
+  expect_equal(create.derived.variables(
+    in.phenotype.data,
+    in.variable.summary
+  ),
+  list(
+    phenotype.data = out.phenotype.data,
+    variable.summary = out.variable.summary
+  ),
+  tolerance = 1e-5
+  )
+})
+
+test_that("rank normal transform stratifies by two variables", {
+  tv0001 <- c(1, 2, 3, 4, 5, 6)
+  tvstrat1 <- factor(c(1, 1, 1, 2, 2, 2))
+  tvstrat2 <- factor(c(1, 1, 2, 2, 3, 3))
+  set1 <- c(-1.3829941, -0.6744898)
+  set2 <- c(-0.2104284, 0.2104284)
+  set3 <- -1 * set1[2:1]
+  targets <- list(set1, set3, set2, set2, set1, set3)
+  n.random.mismatch <- 0
+  n.total <- 100
+  hit.first.entry.counts <- rep(0, 6)
+  hit.any.entry.counts <- rep(0, 6)
+  for (i in seq_len(n.total)) {
+    y <- derive.rank.normal.transform(tv0001,
+      strat = list(tvstrat1, tvstrat2)
+    )
+    for (index in seq_len(6)) {
+      if (abs(y[index] - targets[[index]][1]) < 1e-5 |
+        abs(y[index] - targets[[index]][2]) < 1e-5) {
+        hit.any.entry.counts[index] <- hit.any.entry.counts[index] + 1
+      }
+      if (abs(y[index] - targets[[index]][1]) < 1e-5) {
+        hit.first.entry.counts[index] <- hit.first.entry.counts[index] + 1
+      }
+    }
+  }
+  expect_equal(hit.any.entry.counts, rep(n.total, 6))
+  for (index in seq_len(6)) {
+    expect_true(binom.test(hit.first.entry.counts[index],
+      n.total,
+      0.5,
+      alt = "two"
+    )$p.value >= 0.01)
+  }
+})
+
+test_that("rank normal transform detects when stratification variables are not in a list", {
+  expect_error(derive.rank.normal.transform(1:20,
+    stratification.vars = 1:20
+  ))
+  expect_error(derive.rank.normal.transform(1:20,
+    stratification.vars = "XYY"
+  ))
+})
+
+test_that("rank normal transform detects when offset is malformed", {
+  expect_error(derive.rank.normal.transform(1:20,
+    offset = "abc"
+  ))
+})
+
+test_that("rank normal transform detects when include.subjects is malformed", {
+  expect_error(derive.rank.normal.transform(1:20,
+    include.subjects = rep(TRUE, 19)
+  ))
+})
+
+test_that("rank normal transform detects when primary.call is malformed", {
+  expect_error(derive.rank.normal.transform(1:20,
+    primary.call = rnorm(10)
+  ))
+})
