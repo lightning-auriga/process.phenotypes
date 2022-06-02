0e2a5420d3982febe88de09e7d14989b38a9aee2
Bari J. Ballew <bari.ballew@gmail.com>
2022-06-02 11:18:23 -0400

    docs: add global package documentation

---
+++ R/process_phenotypes.R
@@ -0,0 +1,15 @@
+#' process.phenotypes: automated phenotype standardization and reporting
+#'
+#' @description
+#'
+#' A package for reproducible phenotype data cleaning and quality control.
+#'
+#' @seealso
+#'
+#' - \url{http://54gene-processphenotypes-docs.s3-website.us-east-2.amazonaws.com/index.html}
+#' - \url{https://gitlab.com/data-analysis5/phenotypes/process.phenotypes}
+#'
+#' @docType package
+#' @name process.phenotypes
+NULL
+#> NULL
