5319aa60938dd294c7d73ed14f43443367d716cb
Bari J. Ballew <bari.ballew@gmail.com>
2022-08-01 16:23:32 -0400

    test: add test for validator to detect malformed shared models

--- tests/testthat/_snaps/validation.md
+++ tests/testthat/_snaps/validation.md
@@ -12,3 +12,12 @@
       	- /variables/HW00003 (#/properties/variables/patternProperties/%5E.*%24/oneOf/1/additionalProperties): must NOT have additional properties
       	- /variables/HW00003 (#/properties/variables/patternProperties/%5E.*%24/oneOf): must match exactly one schema in oneOf
 
+# validator recognizes unacceptable shared models yaml
+
+    Code
+      config.validation(dataset.yaml.valid, shared.models.yaml.invalid,
+        dataset.schema, shared.models.schema)
+    Error <validation_error>
+      1 error validating json:
+      	- /models/yesno (#/properties/models/patternProperties/%5E.*%24/required): must have required property 'type'
+
--- tests/testthat/test_validation.R
+++ tests/testthat/test_validation.R
@@ -1,6 +1,7 @@
 dataset.yaml.valid <- "files/validation/dataset-valid.yaml"
 dataset.yaml.invalid <- "files/validation/dataset-invalid.yaml"
 shared.models.yaml.valid <- "files/validation/shared-models-valid.yaml"
+shared.models.yaml.invalid <- "files/validation/shared-models-invalid.yaml"
 dataset.schema <- system.file("validator/schema.datasets.yaml",
   package = "process.phenotypes"
 )
@@ -29,6 +30,19 @@ test_that("validator recognizes unacceptable dataset yaml", {
   expect_snapshot(config.validation(
     dataset.yaml.invalid,
     shared.models.yaml.valid,
+    dataset.schema,
+    shared.models.schema
+  ),
+  error = TRUE
+  )
+})
+
+
+test_that("validator recognizes unacceptable shared models yaml", {
+  skip_if(shared.models.schema == "")
+  expect_snapshot(config.validation(
+    dataset.yaml.valid,
+    shared.models.yaml.invalid,
     dataset.schema,
     shared.models.schema
   ),
