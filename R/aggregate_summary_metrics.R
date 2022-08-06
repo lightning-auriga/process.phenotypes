5282f770b56844766b34807d2de3aa1cabd0f10f
Lightning Auriga <palmer.cam@gmail.com>
2022-08-06 09:04:52 -0400

    docs: update roxygen blocks for cran style compliance

--- R/aggregate_summary_metrics.R
+++ R/aggregate_summary_metrics.R
@@ -1,3 +1,4 @@
+#' @title
 #' Find, for each subject, the number of variables for
 #' which that subject has data of the wrong type.
 #'
@@ -13,10 +14,18 @@
 #' for subject-specific data corruption, so they can
 #' be removed before downstream analysis.
 #'
-#' @param variable.summary list, variable configuration and
-#' summary data
-#' @return named integer vector; values are counts of non-compliant
-#' variables per subject, names are subject IDs
+#' @param variable.summary List; variable configuration and
+#' summary data containing precomputed information about subjects
+#' with unexpected type data for each variable.
+#' @return Named integer vector, where values are counts of non-compliant
+#' variables per subject, and names are subject IDs.
+#' @seealso apply.type.conversions
+#' @examples
+#' var.sum <- list(variables = list(
+#'   HW00001 = list(subjects.wrong.type = c("A")),
+#'   HW00002 = list(subjects.wrong.type = c("A", "B"))
+#' ))
+#' all.bad.subjects <- aggregate.subjects.wrong.type(var.sum)
 aggregate.subjects.wrong.type <- function(variable.summary) {
   all.instances <- unlist(lapply(variable.summary$variables, function(i) {
     i$subjects.wrong.type
@@ -28,6 +37,7 @@ aggregate.subjects.wrong.type <- function(variable.summary) {
   res
 }
 
+#' @title
 #' Find, for each variable, the number of subjects with entries
 #' not matching the specified type.
 #'
@@ -43,10 +53,18 @@ aggregate.subjects.wrong.type <- function(variable.summary) {
 #' in questionnaire content midstream, or potentially configuration
 #' failures in this software.
 #'
-#' @param variable.summary list, variable configuration and
-#' summary data
-#' @return named integer vector; values are counts of non-compliant
-#' subjects per variable, names are variable names
+#' @param variable.summary List; variable configuration and
+#' summary data containing precomputed information about subjects
+#' with unexpected type data for each variable.
+#' @return Named integer vector, where values are counts of non-compliant
+#' subjects per variable, and names are variable names.
+#' @seealso apply.type.conversions
+#' @examples
+#' var.sum <- list(variables = list(
+#'   HW00001 = list(subjects.wrong.type = c("A", "B")),
+#'   HW00002 = list(subjects.wrong.type = character())
+#' ))
+#' wrong.type.counts <- aggregate.variables.wrong.type(var.sum)
 aggregate.variables.wrong.type <- function(variable.summary) {
   counts <- sapply(variable.summary$variables, function(i) {
     length(i$subjects.wrong.type)
@@ -55,7 +73,8 @@ aggregate.variables.wrong.type <- function(variable.summary) {
   counts
 }
 
-#' Compute per-subject NA count across all variables
+#' @title
+#' Compute per-subject NA count across all variables.
 #'
 #' @description
 #' Computes a count for each subject of all NAs across
@@ -68,12 +87,30 @@ aggregate.variables.wrong.type <- function(variable.summary) {
 #' phenomena, including expected blockwise NA due to individual
 #' subjects not completing all questionnaire sections.
 #'
-#' @param phenotype.data data frame, subject data, subjects
-#' as rows, phenotypes as columns
-#' @param variable.summary list, variable configuration
-#' and summary data
-#' @return named integer vector; values are counts of NAs
-#' for each subject across all variables, names are subject IDs
+#' @param phenotype.data Data frame of subject data, with subjects
+#' as rows and phenotypes as columns.
+#' @param variable.summary List; variable configuration and
+#' summary data with subject ID variable annotated as `subject_id = TRUE`.
+#' @return Named integer vector, where values are counts of NAs
+#' for each subject across all variables, and names are subject IDs.
+#' @seealso apply.type.conversions
+#' @examples
+#' pheno.data <- data.frame(
+#'   HW00001 = c("A", "B", "C", "D"),
+#'   HW00002 = c(1, 2, NA, 3)
+#' )
+#' var.sum <- list(variables = list(
+#'   HW00001 = list(
+#'     name = "name1",
+#'     type = "string",
+#'     subject_id = TRUE
+#'   ),
+#'   HW00002 = list(
+#'     name = "name2",
+#'     type = "string"
+#'   )
+#' ))
+#' subject.na.counts <- compute.subject.na.count(pheno.data, var.sum)
 compute.subject.na.count <- function(phenotype.data, variable.summary) {
   res <- apply(phenotype.data, 1, function(i) {
     length(which(is.na(i)))
@@ -81,6 +118,7 @@ compute.subject.na.count <- function(phenotype.data, variable.summary) {
   names(res) <- phenotype.data[, find.subject.id.index(variable.summary)]
   res
 }
+#' @title
 #' Compute per-subject dependency failures counts across all variables
 #'
 #' @description
@@ -89,12 +127,12 @@ compute.subject.na.count <- function(phenotype.data, variable.summary) {
 #' been applied.
 #'
 #' @details
-#' This obviously has a bunch of caveats associated with it.
+#' This obviously has many caveats associated with it.
 #' The metric only means as much as the dependencies do.
 #' Large numbers of dependency failures can be introduced
 #' with particular discrepancies (not addressing atypical
 #' "no" responses in some dependent questions, approximate
-#' BMI deviations, atypical N/A values in string variables)
+#' BMI deviations, atypical N/A values in string variables, etc.)
 #' that will inflate these counts without useful meaning.
 #' Discrepancies in early links in some dependency chains
 #' can cause the same subject to fail an entire batch
@@ -103,11 +141,24 @@ compute.subject.na.count <- function(phenotype.data, variable.summary) {
 #' due to this phenomenon, it's not entirely clear
 #' how many times that penalty should be applied.
 #'
-#' @param variable.summary list, variable configuration
-#' and summary data
-#' @return named integer vector; values are counts of
+#' Dependency enforcement is envisioned for unstructured
+#' upstream data sources. In the case that input data are
+#' generated from some sort of formal data system upstream
+#' (e.g. SurveyCTO), dependencies may already be enforced,
+#' in which case the dependency system in process.phenotypes
+#' is somewhat redundant, thought still a nice sanity
+#' check in the cleaning report.
+#'
+#' @param variable.summary List; variable configuration and
+#' summary data containing precomputed dependency results.
+#' @return Named integer vector, where values are counts of
 #' dependency failures for each subject across all variables,
-#' names are subject IDs
+#' and names are subject IDs.
+#' @seealso check.variable.dependencies
+#' @examples
+#' dep.results <- list("1" = c("A", "B"), "2" = c("C", "D"))
+#' var.sum <- list(variables = list(HW00001 = list(dependency.results = dep.results)))
+#' dep.failures <- aggregate.subject.dep.failures(var.sum)
 aggregate.subject.dep.failures <- function(variable.summary) {
   all.instances <- unlist(lapply(variable.summary$variables, function(i) {
     unique(unlist(i$dependency.results))
