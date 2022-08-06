de06a7ce0296c647e7b1846cb27db3355cfa4886
Lightning Auriga <palmer.cam@gmail.com>
2022-08-06 10:09:03 -0400

    docs: update roxygen blocks for cran style compliance

--- R/create_phenotype_report.R
+++ R/create_phenotype_report.R
@@ -1,39 +1,177 @@
-#' Create markdown report summarizing phenotype data in file
+#' @title
+#' Create markdown report summarizing phenotype data and cleaning process.
 #'
 #' @description
-#' Given phenotype information in tsv file, run reporting/cleaning
-#' and emit summary information.
+#' Given phenotype information in a text file and configuration information
+#' in yaml format, this function runs the primary cleaning logic of the
+#' package, aggregates summary information about the cleaning process,
+#' emits the cleaned data in one of various output formats, and summarizes
+#' the cleaning process in an html report.
 #'
 #' @details
 #' This function is the main entry point for phenotype data cleaning.
-#' For details on exactly what cleaning is performed, please see
+#' The primary cleaning process proceeds as follows:
+#'
+#'  - load dataset and shared model configuration
+#'  - load phenotype data
+#'  - map phenotype headers based on input configuration
+#'    - errors about mismatching column headers are detected here, and
+#'      generally indicate desync between input configuration and dataset.
+#'      this is particularly problematic with iterative runs of SurveyCTO
+#'      data, where columns get injected into the output based on the
+#'      number of responses to repeat variables.
+#'  - make all inputs lowercase
+#'  - remove superfluous whitespace
+#'  - collapse certain types of character repeats
+#'  - apply mappings of Unicode characters to more widely tolerated representations.
+#'    - this is a particularly meaningful step for some datasets. if you're curious
+#'      about the kinds of chaos we've encountered in datasets, feel free to look
+#'      at system.file("unicode_pattern_replacements.tsv", package = "process.phenotypes")
+#'  - exclude patterns matching known Excel error codes.
+#'    - this processing step will be specifically reported in the output report
+#'      as a possible indicator of data malformation. Excel (Calc, Sheets, etc.) should generally not
+#'      be used to process biological data at any step. for more information on why,
+#'      please see \url{https://www.nature.com/articles/d41586-021-02211-4}.
+#'  - detect remaining Unicode characters
+#'    - these are reported in the cleaning report html. Unicode characters
+#'      are permitted in the output dataset, but in general we've found that
+#'      their inclusion is often erroneous, and may potentially conflict with
+#'      harmonization of responses across multiple subjects. if Unicode characters
+#'      are reported in your cleaning report, please consider extending the mapping
+#'      data mentioned above to improve the cleaning process.
+#'  - remove certain nonword characters
+#'  - normalize missing data based on recognized patterns
+#'  - apply consent inclusion and exclusion, if configured
+#'  - normalize missing data based on user-configured patterns
+#'  - apply type conversions based on user-configured types
+#'  - apply age exclusion
+#'  - remove subjects with empty subject IDs
+#'  - apply user-configured bounds to numeric variables
+#'  - if `subject_ancestry = TRUE` is found for any variables in the user configuration,
+#'    apply heuristic harmonization with backend annotations
+#'    - this is currently configured to run versus an _ad hoc_ set of Nigerian
+#'      ancestry groups, and should not be used in other cases without first
+#'      creating a reference list and adding it to the package. if there's sufficient
+#'      interest, this methodology may be expanded and improved
+#'  - if `derived` block is present in user configuration, create derived
+#'    variables from cleaned primary variables
+#'  - apply bounds on derived numeric variables
+#'  - check user-configured dependencies between variables
+#'  - if dependency failures were detected, apply user-configured handling on failures
+#'  - aggregate summary information about: distributions, NA conversions, etc., for html report
+#'  - remove subjects that fail NA rate limits
+#'    - this is not much used currently, but is intended to flag truly toxic
+#'      subjects that may in fact represent catastrophic row or column shifts in
+#'      the input data. please be extremely cautious if any input subjects are removed
+#'      based on this criterion
+#'  - emit cleaning report as html
+#'  - if requested, emit configuration data in yaml format
+#'  - if requested, emit cleaned data as tsv or in formats for other stats languages
+#'
+#' The actual functionality and configuration in this package is extensive and frequently
+#' expanding. Full details on exactly what cleaning is performed and available
+#' configuration options for the input data are available at
 #' \url{http://54gene-processphenotypes-docs.s3-website.us-east-2.amazonaws.com/index.html}
 #'
-#' @param in.filename character vector, name of input phenotype tsv file
-#' @param dataset.yaml character vector, yaml configuration for project
-#' @param shared.model.yaml character vector, yaml configuration for shared model specifications
-#' @param out.filename character vector, name of output report html file
-#' @param quote character vector, character used to quote string tokens; defaults to null
-#' @param sep character vector, character used to delimit input fields; defaults to tab
-#' @param uniq.var.inclusion.prop numeric, proportion of total values of a string
-#' variable that can be unique before tabular output is suppressed from the output report
-#' @param write.tsv logical, whether to emit output phenotype data in tsv tab-delimited plaintext
-#' @param write.stata logical, whether to emit output phenotype data in STATA .dta format
-#' @param write.spss logical, whether to emit output phenotype data in SPSS .zsav format
-#' @param write.sas logical, whether to emit output phenotype data in SAS .sas7bdat format,
-#' along with a source .sas file that needs to be run to assign category levels and types
-#' @param write.yaml logical, whether to emit final version of stored configuration data
-#' in YAML format; currently not tested
-#' @seealso run.experiment
-#' @keywords phenotypes
+#' @param in.filename Character vector fileaname of input phenotype data.
+#' @param dataset.yaml character vector filename of the yaml-format configuration
+#' data for the dataset.
+#' @param shared.model.yaml Character vector filename of the yaml configuration
+#' for shared model specifications used by the dataset configuration.
+#' @param out.filename Character vector filename of output report html file.
+#' @param quote Character vector used to quote string tokens in input phenotypes,
+#' and passed to read.table. Defaults to NULL. This parameter is exposed for
+#' greater compability with unpredictable input formats (see Details).
+#' @param sep Character vector used to delimit input fields in input phenotypes,
+#' and passed to read.table. Defaults to tab (\t). This parameter is exposed for
+#' greater compatibility with unpredictable input formats (see Details).
+#' @param uniq.var.inclusion.prop Numeric proportion of total values of a string
+#' variable that can be unique before tabular output is automatically suppressed
+#' from the output report. If set to a value greater than 1, all variables are
+#' reported; if set below 0, all variable reporting is suppressed.
+#' @param write.tsv Logical indicating whether to emit output phenotype data in tsv
+#' tab-delimited plaintext. This is the primary intended output control for the
+#' function, and should probably be set to TRUE.
+#' @param write.stata Logical indicating whether to emit output phenotype data in
+#' STATA .dta format. This method was added experimentally in order to provide
+#' useful output formats for users of other statistical languages, and is subject
+#' to modification based on downstream user feedback.
+#' @param write.spss Logical indicating whether to emit output phenotype data
+#' in SPSS .zsav format. This method was added experimentally in order to provide
+#' useful output formats for users of other statistical languages, and is subject
+#' to modification based on downstream user feedback.
+#' @param write.sas Logical indicating whether to emit output phenotype data
+#' in SAS .sas7bdat format, along with a source .sas file that needs to be run to
+#' assign category levels and types. This method was added experimentally in order
+#' to provide useful output formats for users of other statistical languages,
+#' and is subject to modification based on downstream user feedback.
+#' @param write.yaml Logical indicating whether to emit final version of stored
+#' configuration data in YAML format. In addition to general recordkeeping, the
+#' goal of this output is to provide a configuration file that can be used to
+#' reload the output tsv back into process.phenotypes.
 #' @export create.phenotype.report
 #' @examples
-#' \dontrun{
-#' create.phenotype.report(
-#'   "/path/to/directory/MM_FINAl_store.tsv",
-#'   "MM", "/output/path/MM_report.html"
+#' ## create.phenotype.report operates on input files, which can be constructed
+#' ## from dataframes and lists in R if desired.
+#' input.phenotypes <- tempfile("cpr.input.data", fileext = ".tsv")
+#' input.dataset.yaml <- tempfile("cpr.input.data", fileext = ".dataset.yaml")
+#' input.shared.models <- tempfile("cpr.input.data", fileext = ".shared_models.yaml")
+#' output.html <- tempfile("cpr.output.data", fileext = ".html")
+#' ## a minimal dataset has to contain at least a subject ID column and
+#' ## a subject age column. everything else is optional.
+#' pheno.data <- data.frame(
+#'   var1 = c("A", "B", "C", "D", "E"),
+#'   var2 = 16:20,
+#'   var3 = sample(c("yes", "no"), 5, replace = TRUE)
 #' )
-#' }
+#' var.summary <- list(
+#'   globals = list(
+#'     consent_inclusion_file = NULL,
+#'     consent_exclusion_file = NULL,
+#'     max_invalid_datatypes_per_subject = 10,
+#'     min_age_for_inclusion = 18
+#'   ),
+#'   variables = list(
+#'     HW00001 = list(
+#'       name = "var1",
+#'       type = "string",
+#'       suppress_reporting = TRUE,
+#'       subject_id = TRUE
+#'     ),
+#'     HW00002 = list(
+#'       name = "var2",
+#'       type = "numeric",
+#'       subject_age = TRUE
+#'     ),
+#'     HW00003 = list(
+#'       name = "var3",
+#'       shared_model = "yesno"
+#'     )
+#'   )
+#' )
+#' shared.models <- list(models = list("yesno" = list(
+#'   type = "categorical",
+#'   levels = list(
+#'     "1" = list(name = "no"),
+#'     "2" = list(name = "yes")
+#'   )
+#' )))
+#' write.table(pheno.data, input.phenotypes,
+#'   row.names = FALSE,
+#'   col.names = TRUE, quote = TRUE, sep = "\t"
+#' )
+#' yaml::write_yaml(var.summary, input.dataset.yaml)
+#' yaml::write_yaml(shared.models, input.shared.models)
+#' ## output datasets take their output prefix from the filename provided for the html report,
+#' ## so for example purposes the other output formats are suppressed.
+#' create.phenotype.report(input.phenotypes,
+#'   input.dataset.yaml,
+#'   input.shared.models,
+#'   output.html,
+#'   sep = "\t",
+#'   quote = "\"",
+#'   write.tsv = FALSE
+#' )
 #' @importFrom stats qnorm quantile sd
 #' @importFrom utils read.table type.convert write.table
 #' @importFrom magrittr `%>%`
