bc881a35de034ddb97eae6d8340b8131a3af4f36
Lightning Auriga <palmer.cam@gmail.com>
2022-08-09 20:12:13 -0400

    docs: update roxygen blocks for cran style compliance

--- R/remove_invalid_columns.R
+++ R/remove_invalid_columns.R
@@ -1,3 +1,4 @@
+#' @title
 #' Remove bad columns from a phenotype data frame
 #'
 #' @description
@@ -5,15 +6,22 @@
 #' - column name is NA
 #' - column name is empty string
 #'
-#' @param df data frame, input phenotype content
-#' @return modified version of input with certain bad columns
-#' removed
+#' @details
+#' This function is a legacy of the earliest and messiest
+#' datasets we encountered during the development of this
+#' package. If you're lucky, such columns will not manifest
+#' themselves in your dataset at all. If you're unlucky
+#' and you have malformed headers, please be cautious:
+#' in our experience, this tends to be a proxy indicator
+#' for many types of pernicious row shifting.
+#'
+#' @param df Data frame of input phenotype content with column headers.
+#' @return Data frame containing modified version of input with
+#' relevant bad columns removed.
 #' @examples
-#' \dontrun{
 #' phenotype.data <- data.frame(1:3, 4:6, 7:9)
 #' colnames(phenotype.data) <- c("", NA, "ok name")
-#' phenotype.data <- remove.invalid.columns(phenotype.data)
-#' }
+#' phenotype.data <- process.phenotypes:::remove.invalid.columns(phenotype.data)
 remove.invalid.columns <- function(df) {
   res <- df
   res[, which(colnames(res) == "" | is.na(colnames(res)))] <- NULL
