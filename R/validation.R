16e4a14ae9d94fab2bf1dc326e641fc917652c4b
Lightning Auriga <lightning.auriga@gmail.com>
2022-08-16 10:13:08 -0400

    docs: update roxygen block for cran style compliance

--- R/validation.R
+++ R/validation.R
@@ -1,4 +1,19 @@
+#' @title
 #' Run schema validation for process.phenotypes configuration files
+#'
+#' @description
+#' This function takes json schema files specifying the permitted formats
+#' of process.phenotypes dataset and shared model configuration files,
+#' and uses them to validate a provided pair of user configuration files.
+#' If the schema validation is successful, an acknowledgment message
+#' will be printed for each input; if not, the embedded validator will
+#' report a somewhat helpful summary of the issue to guide the process
+#' of fixing the configuration.
+#'
+#' @details
+#' This function replaces the old python validator that was inherited
+#' from a very old version of this package, and that was removed some
+#' time ago.
 #'
 #' The function returns NULL, and is run primarily for its side effect
 #' of emitting verbose logging data about the manner of validation
@@ -8,7 +23,19 @@
 #' @param shared.models.yaml character; name of input shared models yaml file
 #' @param dataset.schema character; name of schema specification for dataset yaml
 #' @param shared.models.schema character; name of schema specification for shared models yaml
+#' @return NULL
 #' @export
+#' @examples
+#' dataset.yaml <- system.file("examples", "example.dataset.yaml", package = "process.phenotypes")
+#' shared.models.yaml <- system.file("examples", "example.shared_models.yaml", package = "process.phenotypes")
+#' dataset.schema <- system.file("validator", "schema.datasets.yaml", package = "process.phenotypes")
+#' shared.models.schema <- system.file("validator", "schema.shared-models.yaml", package = "process.phenotypes")
+#' process.phenotypes::config.validation(
+#'   dataset.yaml,
+#'   shared.models.yaml,
+#'   dataset.schema,
+#'   shared.models.schema
+#' )
 config.validation <- function(dataset.yaml,
                               shared.models.yaml,
                               dataset.schema,
